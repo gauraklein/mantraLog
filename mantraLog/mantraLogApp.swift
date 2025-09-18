f0265720b922cc8b6fbfc1fda70e5cb352933afc
Gaura Klein <gaurakklein@gmail.com>
2025-09-18 15:32:46 -0500

    Initial Commit

---
+++ mantraLog/mantraLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  mantraLogApp.swift
+//  mantraLog
+//
+//  Created by Gaura Klein on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct mantraLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mantraLogTests/mantraLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  mantraLogTests.swift
+//  mantraLogTests
+//
+//  Created by Gaura Klein on 9/18/25.
+//
+
+import Testing
+@testable import mantraLog
+
+struct mantraLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
